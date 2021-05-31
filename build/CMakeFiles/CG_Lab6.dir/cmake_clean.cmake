00c0cb2cb6de1099b0a8be2137ce869bee5ff353
Fedor Miron <fedormiron1@ya.ru>
2021-05-31 15:05:13 +0300

    initial

---
+++ build/CMakeFiles/CG_Lab6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CG_Lab6"
+  "CG_Lab6.pdb"
+  "CMakeFiles/CG_Lab6.dir/main.cpp.o"
+  "CMakeFiles/CG_Lab6.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CG_Lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
